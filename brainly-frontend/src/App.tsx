affe6800778a006ce450ab29be24392a855d049a
Abhinav <abhichaurasia016@gmail.com>
2025-08-21 17:42:41 +0530

    minor errors updated

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="" element={<Signup/>}/>
         <Route path="/share/:shareId" element={<SharePage/>}/>
         <Route path="/universe" element={<Dashboard/>}/>
         <Route path="/youtube" element={<YoutubePage/>}/>
